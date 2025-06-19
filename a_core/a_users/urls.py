a966d0b508b4648501e33ab939e6e0c24f7c4e91
yamilobt8 <yamilo12345678910@gmail.com>
2025-06-19 04:36:31 +0100

    separing between the users app urls and the main app urls

--- a_core/a_users/urls.py
+++ a_core/a_users/urls.py
@@ -2,9 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    #path('register', views.register_view, name='register'),
     path('login', views.login_view, name='login'),
     path('register', views.register_user, name='register'),
     path('ajax/send-otp/', views.send_otp, name='send_otp'),
     path('ajax/verify-otp/', views.verify_otp, name='verify_otp'),
+    path('logout', views.logout_view, name='logout'),
 ]
