2143782e57b012241abd245890522226a3405357
yamilobt8 <yamilo12345678910@gmail.com>
2025-06-19 04:35:28 +0100

    Completed the login and logout functions

--- a_core/a_users/views.py
+++ a_core/a_users/views.py
@@ -1,50 +1,31 @@
 import json
 from django.shortcuts import render, redirect
-from django.http import HttpResponse
 from .forms import RegisterForm, LoginForm
 from django.contrib.auth.models import User
 from django.core.mail import send_mail
 import random
 from django.http import JsonResponse
-
-
-
-#def register_view(request):
-#    if request.method == 'POST':
-#        form = RegisterForm(request.POST)
-#        if form.is_valid():
- #           username = form.cleaned_data.get('username')
-#            email = form.cleaned_data.get('email')
-#            password = form.cleaned_data.get('password')
-#
-#            if User.objects.filter(email=email).exists():
-#                form.add_error('email', 'Enter a valid email address.')
-#            elif User.objects.filter(username=username).exists():
-#                form.add_error('username', 'Username already registered.')
- #           else:
- #               User.objects.create_user(username, email, password)
- #               send_email(request, email)
- #               return redirect('home')
-#        else:
- #           print(form.errors)
-#    else:
- #       form = RegisterForm()
- #   return render(request, 'a_users/register.html', {'form': form})
+from django.contrib.auth import authenticate, login, logout
 
 def login_view(request):
     if request.method == 'POST':
-        form = LoginForm(request.POST)
-        if form.is_valid():
-            email = form.cleaned_data.get('email')
-            # form.add_error('email', 'enter a valid email address.')
-            password = form.cleaned_data.get('password')
-        else:
-            print(form.errors)
-            print('here')
-    else:
-        print('not post request')
-        form = LoginForm()
+        print(request.body)
+        try:
+            data = json.loads(request.body)
+        except json.JSONDecodeError:
+            return JsonResponse({'error': 'Invalid JSON'}, status=400)
 
+        username = data.get('username', '').strip()
+        password = data.get('password', '').strip()
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return JsonResponse({'message': 'success'})
+        else:
+            return JsonResponse({'message': 'Invalid username or password'})
+
+    form = LoginForm()
     return render(request, 'a_users/login.html', {'form': form})
 
 def send_email(request, recipient_email, subject):
@@ -55,7 +36,6 @@ def send_email(request, recipient_email, subject):
         recipient_list=[recipient_email],
         fail_silently=False,
     )
-
 def send_otp(request):
     if request.method == 'POST':
         form = RegisterForm(request.POST)
@@ -74,9 +54,7 @@ def send_otp(request):
             print(f"Sending OTP {otp} to {email}")
             return JsonResponse({'message': 'OTP sent Successfully'})
     return JsonResponse({'error': 'Invalid request method.'}, status=405)
-
 def verify_otp(request):
-    global otp_verified
     if request.method == 'POST':
         try:
             data = json.loads(request.body)
@@ -90,7 +68,6 @@ def verify_otp(request):
             return JsonResponse({'message': 'OTP verified'})
         return JsonResponse({'message': 'Invalid OTP'})
     return JsonResponse({'error': 'Invalid request method.'}, status=405)
-
 def register_user(request):
     if request.method == 'POST':
         print(request.body)
@@ -104,9 +81,15 @@ def register_user(request):
         else:
             email = data.get('email', '').strip()
             password = data.get('password', '').strip()
-            User.objects.create_user(username=username, email=email, password=password)
+            user = User.objects.create_user(username=username, email=email, password=password)
+            user.save()
+            login(request, user)
             send_email(request, email, f'thank you for registering on our site {username}')
             return JsonResponse({'message': 'User registered successfully'})
 
     form = RegisterForm()
     return render(request, 'a_users/register.html', {'form': form})
+
+def logout_view(request):
+    logout(request)
+    return redirect('home')
