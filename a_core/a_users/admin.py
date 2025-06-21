39bca732aa1f96575b77b62cb5bfb3656176fc1e
yamilobt8 <yamilo12345678910@gmail.com>
2025-06-21 14:35:30 +0100

    register the new db models

--- a_core/a_users/admin.py
+++ a_core/a_users/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from a_users.models import Profile, PasswordHistory
 
 # Register your models here.
+admin.site.register(Profile)
+admin.site.register(PasswordHistory)
