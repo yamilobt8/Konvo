c306e308691e778ca8c25f9250775cc6da6c5c53
yamilobt8 <yamilo12345678910@gmail.com>
2025-06-21 14:33:05 +0100

    manipulating user general infos

---
+++ a_core/a_users/static/a_users/general-infos.js
@@ -0,0 +1,16 @@
+document.addEventListener('DOMContentLoaded', function(event) {
+    event.preventDefault();
+    const toggleLink = document.getElementById('toggleArrow');
+    const icon = toggleLink.querySelector('i');
+    const collapseEl = document.getElementById('collapseForm');
+
+    collapseEl.addEventListener('show.bs.collapse', () => {
+        icon.classList.remove('bi-chevron-right');
+        icon.classList.add('bi-chevron-down');
+    });
+
+    collapseEl.addEventListener('hide.bs.collapse', () => {
+        icon.classList.remove('bi-chevron-down');
+        icon.classList.add('bi-chevron-right');
+    });
+})
