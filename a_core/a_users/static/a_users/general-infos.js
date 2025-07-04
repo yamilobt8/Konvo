a1df4459b788d371cef1b8d268e739b9793606c2
yamilobt8 <yamilo12345678910@gmail.com>
2025-07-04 15:31:42 +0100

    fix id selection

--- a_core/a_users/static/a_users/general-infos.js
+++ a_core/a_users/static/a_users/general-infos.js
@@ -2,7 +2,7 @@ document.addEventListener('DOMContentLoaded', function(event) {
     event.preventDefault();
     const toggleLink = document.getElementById('toggleArrow');
     const icon = toggleLink.querySelector('i');
-    const collapseEl = document.getElementById('collapseForm');
+    const collapseEl = document.getElementById('collapseForm1');
 
     collapseEl.addEventListener('show.bs.collapse', () => {
         icon.classList.remove('bi-chevron-right');
