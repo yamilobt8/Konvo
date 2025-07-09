5386b6edf75be811b4b3cc6f9b2cb227cb769a4d
yamilobt8 <yamilo12345678910@gmail.com>
2025-07-09 14:46:59 +0100

    create and view channels path

--- a_core/a_app/urls.py
+++ a_core/a_app/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('about', views.about, name='about'),
+    path('create_channel', views.create_channel, name='create_channel'),
+    path('view_channel/<int:channel_id>', views.view_channel, name='view_channel'),
 ]
