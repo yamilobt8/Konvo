762e89eaeb379ecd2167edbbe41fd99a31372b9c
yamilobt8 <yamilo12345678910@gmail.com>
2025-07-09 14:47:37 +0100

    Channel Model

--- a_core/a_app/models.py
+++ a_core/a_app/models.py
@@ -1,3 +1,16 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class Channel(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name='owned_channels')
+    # user.owned_channels.all()
+    subscribers = models.ManyToManyField(User, related_name='subscribed_channels', blank=True)
+    # user.subscribed_channels.all()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+    def __str__(self):
+        return self.title
