b2fa269dc44111b273daace8b77d325ea4963fd2
yamilobt8 <yamilo12345678910@gmail.com>
2025-07-09 03:02:34 +0100

    added the channel form

--- a_core/a_app/forms.py
+++ a_core/a_app/forms.py
@@ -1,3 +1,5 @@
 from django import forms
 
-
+class ChannelForm(forms.Form):
+    title = forms.CharField(label="Title", max_length=16, widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Entre Channel Title'}))
+    description = forms.CharField(label="Description", max_length=200, widget=forms.Textarea(attrs={'class': 'form-control', 'placeholder': 'Entre Channel Description', 'rows': 7}))
